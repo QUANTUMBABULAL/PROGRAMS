0f4888729bf894eef1d69ccbc283dbd57b34c37f
Babulal100 <mecaroostermrc@gmail.com>
2026-06-15 23:23:06 +0530

    Solved LC/7.py

--- LC/6.py
+++ LC/6.py
@@ -1,3 +1,12 @@
+# class Solution(object):
+#     def intersection(self, nums1, nums2):
+#         return list(set(nums1) & set(nums2))
+
+
 class Solution(object):
-    def intersection(self, nums1, nums2):
-        return list(set(nums1) & set(nums2))
+    def intersection(self,nums1,nums2):
+        return list(set(nums1)&set(nums2))
+    
+
+
+    
