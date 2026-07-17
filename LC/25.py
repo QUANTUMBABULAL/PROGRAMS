dc155e5f8b317e605b36ea1b88136f0c81a15677
Babulal100 <mecaroostermrc@gmail.com>
2026-07-17 23:52:10 +0530

    done another

--- LC/25.py
+++ LC/25.py
@@ -1,1 +1,14 @@
-123
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        # Sum of the first window
+        window_sum = sum(nums[:k])
+        max_sum = window_sum
+
+        # Slide the window
+        for i in range(k, len(nums)):
+            window_sum += nums[i]      # Add new element
+            window_sum -= nums[i - k]  # Remove old element
+
+            max_sum = max(max_sum, window_sum)
+
+        return max_sum / k
