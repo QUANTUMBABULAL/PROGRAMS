77df4da5657dcca7271093702a06e584f4265a40
Babulal100 <mecaroostermrc@gmail.com>
2026-08-06 09:27:54 +0530

    Solved HACR/6.c

---
+++ HACR/6.c
@@ -0,0 +1,60 @@
+// Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+
+// Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+
+ 
+
+// Example 1:
+
+// Input: s = "abciiidef", k = 3
+// Output: 3
+// Explanation: The substring "iii" contains 3 vowel letters.
+// Example 2:
+
+// Input: s = "aeiou", k = 2
+// Output: 2
+// Explanation: Any substring of length 2 contains 2 vowels.
+// Example 3:
+
+// Input: s = "leetcode", k = 3
+// Output: 2
+// Explanation: "lee", "eet" and "ode" contain 2 vowels.
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s consists of lowercase English letters.
+// 1 <= k <= s.length
+#include<string.h>
+int isvovel(char ch){
+    return (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u');
+}
+int maxVowels(char* s, int k) {
+    int right=0;
+    int left=0;
+    int count=0;
+    int max=0;
+    while(right<strlen(s)){
+        if(isvovel(s[right])){
+            count++;
+        }
+        while(right-left+1>k){
+            if(isvovel(s[left])){
+                count--;
+
+            }
+            left++;
+        }
+        if(right-left+1==k){
+            if(count>max){
+                max=count;
+            }
+        }
+        
+        right++;
+    }
+    return max;
+
+
+}
