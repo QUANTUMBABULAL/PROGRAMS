a86d448c9f355f4fbc86def07afa87bff565895e
Babulal100 <mecaroostermrc@gmail.com>
2026-08-14 23:43:58 +0530

    Solved CPP_LC/49.cpp

---
+++ CPP_LC/49.cpp
@@ -0,0 +1,68 @@
+/*
+49. Group Anagrams
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+Example 2:
+
+Input: strs = [""]
+
+Output: [[""]]
+
+Example 3:
+
+Input: strs = ["a"]
+
+Output: [["a"]]
+
+ 
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letter
+*/
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<vector<string>>> mp;
+        for(int i=0;i<strs.size();i++){
+            string  ele =strs[i];
+            sort(ele.begin(),ele.end());
+            if(mp.find(ele)!=mp.end()){
+                mp[ele].push_back(strs[i]);
+            }
+            mp[ele]={strs[i]};
+
+        }
+        vector<vector<string>> ans;
+        for(auto p:mp){
+            ans.push_back(p[second]);
+        }
+        return  ans;
+    }
+};
