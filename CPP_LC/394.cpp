cc1400fa484a911ecf3ccd88e6c32c0c442582fb
Babulal100 <mecaroostermrc@gmail.com>
2026-08-24 23:39:01 +0530

    Solved CPP_LC/394.cpp

---
+++ CPP_LC/394.cpp
@@ -0,0 +1,74 @@
+/*
+394. Decode String
+Medium
+Topics
+premium lock icon
+Companies
+Given an encoded string, return its decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+You may assume that the input string is always valid; there are no extra white spaces, square brackets are well-formed, etc. Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there will not be input like 3a or 2[4].
+
+The test cases are generated so that the length of the output will never exceed 105.
+
+ 
+
+Example 1:
+
+Input: s = "3[a]2[bc]"
+Output: "aaabcbc"
+Example 2:
+
+Input: s = "3[a2[c]]"
+Output: "accaccacc"
+Example 3:
+
+Input: s = "2[abc]3[cd]ef"
+Output: "abcabccdcdcdef"
+ 
+
+Constraints:
+
+1 <= s.length <= 30
+s consists of lowercase English letters, digits, and square brackets '[]'.
+s is guaranteed to be a valid input.
+All the integers in s are in the range [1, 300].
+*/
+#include <cctype>
+#include  <string>
+#include <stack>
+class Solution {
+public:
+    string decodeString(string s) {
+        int num =0;
+        string curr ="";
+        stack <string>  stringst;
+        stack <int>  numst;
+        //case 1
+        for(int i =0; i<s.size();i++){
+        if(isdigit(s[i])){
+            num = num*10+(s[i]-'0');
+        }else if(s[i]=='['){
+            stringst.push(curr);
+            numst.push(num);
+            num =0;
+            curr="";
+        }else if(s[i]==']'){
+            int previous_num=numst.top();
+            numst.pop();
+            string previous_str=stringst.top();
+            stringst.pop();
+            string temp ="";
+            for(int j=0;j<previous_num;j++){
+                temp+=previous_str;
+            }
+    curr = previous_str + temp;
+        }else{
+            curr+=s[i];
+        }
+
+        }
+        return curr;
+    }
+};
